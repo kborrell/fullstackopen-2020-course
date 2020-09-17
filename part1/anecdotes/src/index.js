cd8c0edbf944ad411b6f572c8aa75f12443e5987
Kevin Borrell <kborrells@gmail.com>
2020-09-18 00:42:07 +0200

    Completed exercises 1.12-1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,79 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
+const Button = ({handleClick, name}) => {
+  return (
+    <button onClick={handleClick}>{name}</button>
+  )
+}
+
+const Anecdote = ({anecdote, votes}) => {
+  return (
+    <div>
+      <p>{anecdote}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const AnecdoteOfTheDay = ({anecdotes, selected, points, onRandomAnecdote, onVote}) => {
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} votes={points[selected]} />
+      <Button handleClick={() => onVote(selected)} name="vote"/>
+      <Button handleClick={() => onRandomAnecdote(selected)} name="next anecdote"/>
+    </div>
+  )
+}
+
+const TopAnecdote = ({anecdotes, points}) => {
+  const getMostVotedAnecdote = () => {
+    let mostVotes = 0
+    let mostVotedIndex = 0
+
+    for (let index = 0; index < points.length; index++) {
+      const anecdotePoints = points[index];
+      if (anecdotePoints > mostVotes) {
+        mostVotes = anecdotePoints
+        mostVotedIndex = index
+      }
+    }
+
+    return mostVotedIndex
+  }
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={anecdotes[getMostVotedAnecdote()]} votes={points[getMostVotedAnecdote()]} />
+    </div>
+  )
+}
+
+const App = (props) => {
+  var votesArray = Array(6).fill(0)
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(votesArray)
+
+  const getRandomValue = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
+  const selectRandomAnecdote = () => setSelected(getRandomValue(0, props.anecdotes.length))
+
+  const voteAnecdote = (anecdoteIndex) => {
+    let pointsCopy = [...points]
+    pointsCopy[anecdoteIndex] += 1
+    setPoints(pointsCopy)
+  }
+  
+  return (
+    <div>
+      <AnecdoteOfTheDay anecdotes={props.anecdotes} selected={selected} points={points} onRandomAnecdote={selectRandomAnecdote} onVote={voteAnecdote}/>
+      <TopAnecdote anecdotes={props.anecdotes} points={points}/>
     </div>
   )
 }
